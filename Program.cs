879b2ffd22f73b674b77b88cf2910c4196854ecb
Marcin Kijak <kijak76@gmail.com>
2021-04-26 21:04:54 +0200

    Implemented inserting new customer to a file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Fakturowanie
 {
@@ -6,7 +7,32 @@ namespace Fakturowanie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var consoleApplication = new ConsoleApplication();
+
+            consoleApplication.Run();
+
+        }
+    }
+
+    public class ConsoleApplication
+    {
+        public void Run()
+        {
+            Console.WriteLine("Insert Customer name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Insert Customer address:");
+            var address = Console.ReadLine();
+            Console.WriteLine("Insert Customer tax number:");
+            var taxNumber = Console.ReadLine();
+            Console.WriteLine("Insert Customer e-mail:");
+            var eMail = Console.ReadLine();
+            Console.WriteLine("Insert Customer phone number:");
+            var phoneNumber = Console.ReadLine();
+            File.AppendAllText(@"C:\Users\Lenovo\Desktop\R\Customers.txt", $"name:{name} address:{address} taxNumber:{taxNumber} eMail:{eMail} phoneNumber:{phoneNumber}{Environment.NewLine}");
+            //File.ReadAllLines(); sparsować
+
+
         }
     }
 }
